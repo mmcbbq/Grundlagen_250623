09ca62e56d8e1c9f6f740a4d4fddf747713cded8
mmcbbq <m.martinez@bbq.de>
2026-01-12 11:44:28 +0100

    add json encode decode in kea-aufgabe

--- aufgabe/10_dictionarys_aufgaben.py
+++ aufgabe/10_dictionarys_aufgaben.py
@@ -1,14 +1,28 @@
 # Erstelle ein leeres Dictionary namens my_dict.
+my_dict = {}
 # Fülle das Dictionary my_dict mit drei Schlüssel-Wert-Paaren:
 # "Apfel" mit dem Wert 3, "Banane" mit dem Wert 2 und "Orange" mit dem Wert 5.
-
+my_dict['Apfel'] = 3
+my_dict['Banane'] = 2
+my_dict['Orange'] = 5
+my_dict['Traube'] = 4
 # Drucke den Wert für den Schlüssel "Banane" aus.
+print(my_dict['Banane'])
 # Überprüfe, ob der Schlüssel "Traube" im Dictionary my_dict vorhanden ist.
+print('Traube' in my_dict)
 # Füge dem Dictionary my_dict einen neuen Eintrag hinzu: "Traube" mit dem Wert 4.
 # Aktualisiere den Wert für den Schlüssel "Apfel" auf 6.
+my_dict['Apfel'] = 6
 # Lösche den Eintrag für den Schlüssel "Orange" aus dem Dictionary.
-# Drucke alle Schlüssel in my_dict aus.
-# Drucke alle Werte in my_dict aus.
-# Drucke alle Schlüssel-Wert-Paare in my_dict aus
+# del my_dict['Orange']
+my_dict.pop('Orange')
+print(my_dict)
 
+
+# Drucke alle Schlüssel in my_dict aus.
+print(my_dict.keys())
+# Drucke alle Werte in my_dict aus.
+print(my_dict.values())
+# Drucke alle Schlüssel-Wert-Paare in my_dict aus
+print(my_dict.items())
 
--- vorlesen/10.3_nested_lists.py
+++ vorlesen/10.3_nested_lists.py
@@ -1,16 +1,16 @@
 # nested list oder multidimensional array. Listen die Listen enthalten
-nested_list = [1, 2, 3, 'a', 'b', 'c', True, False, True, [1, 2, 3], ['a', 'b', 'c'],
+nested_list = [1, 2, 3, 'a', 'b', 'c', True, False, True, [1, 2, 3], ['a', 'b', 'c',],
 			   [True, False, True, [1, 'b', False]]]
 
 print(nested_list[9])  # -> innere Listen kann ansprechen in dem ich zunächst die äussere Liste anpreche und auf den Index mit der inneren Liste verweise
 print(nested_list[9][1])  # -> die Elemente der inneren Liste kann man wieder mit den Index ansprechen
 print(nested_list[10][0])
-print(nested_list[11][-1])
+print(nested_list[11][3])
 print(nested_list[11][-1][1])
 
 nested_list[10].append('d')  # -> man kann auch alle Listen methoden auf die inneren Listen anwenden
 print(nested_list[10])
-nested_list[11].insert(0, 'bool')
+nested_list[11].insert(0, [False,True])
 nested_list[11][1] = False
 nested_list[11][-1].pop(-2)
 # nested_list.remove('b') #-> Findet keine Elemente der inneren Liste
