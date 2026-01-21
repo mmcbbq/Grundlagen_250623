8cfd6b833149f6ca41aa6f9a43399352fd5a636c
mmcbbq <m.martinez@bbq.de>
2026-01-21 14:13:33 +0100

    balls and special

--- loesungen/11_loop_while_aufgaben_lösungen.py
+++ loesungen/11_loop_while_aufgaben_lösungen.py
@@ -6,8 +6,8 @@ summe = 0
 z = 0
 ende = int(input('Summe Zahl'))
 while z < ende:
-	z += 1
-	summe += z
+    z += 1
+    summe += z
 print(summe)
 
 # Passwort überprüfen:
@@ -22,19 +22,16 @@ password = 'G3h31m'
 pass_check = True
 versuche = 0
 while pass_check:
-	if versuche == 3:
-		print('Das war zu viel')
-		break
-	ant = input('Password')
-	if password == ant:
-		pass_check = False
-	else:
-		versuche += 1
-		print('Das war Falsch nochmal')
+    if versuche == 3:
+        print('Das war zu viel')
+        break
+    ant = input('Password')
+    if password == ant:
+        pass_check = False
+    else:
+        versuche += 1
+        print('Das war Falsch nochmal')
 print('Du bist drin')
-
-
-
 
 # Ein Spieler soll eine (zufällig gewählte) Zahl zwischen 1 und 100 erraten.
 # Das Programm soll jeweils die Meldungen „Die Zahl ist zu groß“,
@@ -43,6 +40,7 @@ print('Du bist drin')
 # Wenn die Zahl zu Gross oder zu klein ist, soll die Frage erneut gestellt werden
 
 import random
+
 zufall_zahl = random.randint(1, 100)
 print(zufall_zahl)
 versuche = 10
@@ -51,21 +49,18 @@ nicht_richtig = True
 
 while nicht_richtig:
 
-	ant = int(input('Zahl zwischen 1 -100'))
-	if ant < zufall_zahl:
-		print('Zu klein')
-	elif ant > zufall_zahl:
-		print('zu Groß')
-	elif ant == zufall_zahl:
-		print('Treffer')
-		nicht_richtig = False
-	versuche -= 1
-	if versuche == 0:
-		print('Keine Versuche mehr')
-		break
-	fver.append(ant)
-	print('Du hast noch ',versuche, fver)
+    ant = int(input('Zahl zwischen 1 -100'))
+    if ant < zufall_zahl:
+        print('Zu klein')
+    elif ant > zufall_zahl:
+        print('zu Groß')
+    elif ant == zufall_zahl:
+        print('Treffer')
+        nicht_richtig = False
+    versuche -= 1
+    if versuche == 0:
+        print('Keine Versuche mehr')
+        break
+    fver.append(ant)
+    print('Du hast noch ', versuche, fver)
 print('Die Zahl war ', zufall_zahl)
-
-
-
